c577a8d02bcb69c8e19d9cea1ad4722c97f14e4e
Chris Chang <christopher_chang@condenast.com>
2021-05-09 14:24:56 -0500

    chore(style): Add Black and style checking (#4)
    * update dev dependencies and add more style checks
    
    * delete obsolete requirements.txt
    
    * add lint checking boilerplate
    
    * black

--- django_test_curl/django_test_curl.py
+++ django_test_curl/django_test_curl.py
@@ -7,13 +7,13 @@ from django.http import HttpResponse
 from django.test import Client as BaseClient
 
 
-curl_parser = argparse.ArgumentParser('Dummy curl parser')
-curl_parser.add_argument('url')
-curl_parser.add_argument('-X', '--request')
-curl_parser.add_argument('-v', '--verbose', action='store_true')
-curl_parser.add_argument('-I', '--head', action='store_true')
-curl_parser.add_argument('-H', '--header', action='append')
-curl_parser.add_argument('-d', '--data')
+curl_parser = argparse.ArgumentParser("Dummy curl parser")
+curl_parser.add_argument("url")
+curl_parser.add_argument("-X", "--request")
+curl_parser.add_argument("-v", "--verbose", action="store_true")
+curl_parser.add_argument("-I", "--head", action="store_true")
+curl_parser.add_argument("-H", "--header", action="append")
+curl_parser.add_argument("-d", "--data")
 
 
 class CurlClientMixin:
@@ -32,16 +32,16 @@ class CurlClientMixin:
         HttpResponse
         """
         curl_cmd, *args = shlex.split(cmd)
-        assert curl_cmd == 'curl'
+        assert curl_cmd == "curl"
         opts = curl_parser.parse_args(args)
 
         if opts.data and not opts.request:
-            opts.request = 'post'
+            opts.request = "post"
         if opts.head and not opts.request:
-            opts.request = 'head'
+            opts.request = "head"
         if not opts.request:
-            opts.request = 'get'
-        assert opts.request in ('head', 'delete', 'get', 'post')
+            opts.request = "get"
+        assert opts.request in ("head", "delete", "get", "post")
 
         url = urlparse(opts.url)
 
@@ -50,20 +50,24 @@ class CurlClientMixin:
         path = url.path
 
         if url.query:
-            path = f'{path}?{url.query}'
+            path = f"{path}?{url.query}"
 
         if url.username and url.password:
-            hashcode = base64.b64encode(f'{url.username}:{url.password}'.encode('utf-8'))
-            kwargs['HTTP_AUTHORIZATION'] = f'Basic {hashcode.decode("utf8")}'
+            hashcode = base64.b64encode(
+                f"{url.username}:{url.password}".encode("utf-8")
+            )
+            kwargs["HTTP_AUTHORIZATION"] = f'Basic {hashcode.decode("utf8")}'
 
-        header_items = [x.split(':', 2) for x in opts.header or []]
-        headers = {'HTTP_' + k.strip().upper().replace('-', '_'): v.strip()
-                   for k, v in header_items}
-        if 'HTTP_CONTENT_TYPE' in headers:
-            kwargs['content_type'] = headers.pop('HTTP_CONTENT_TYPE')
+        header_items = [x.split(":", 2) for x in opts.header or []]
+        headers = {
+            "HTTP_" + k.strip().upper().replace("-", "_"): v.strip()
+            for k, v in header_items
+        }
+        if "HTTP_CONTENT_TYPE" in headers:
+            kwargs["content_type"] = headers.pop("HTTP_CONTENT_TYPE")
 
         if opts.data:
-            kwargs['data'] = opts.data
+            kwargs["data"] = opts.data
 
         return getattr(self, opts.request)(path, **headers, **kwargs)
 
