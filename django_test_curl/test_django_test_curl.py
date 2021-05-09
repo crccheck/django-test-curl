ac36043c2a491fd695eb790fee8577ab9080db76
crccheck <c@crccheck.com>
2021-05-09 10:16:24 -0500

    black

--- django_test_curl/test_django_test_curl.py
+++ django_test_curl/test_django_test_curl.py
@@ -8,43 +8,43 @@ class CurlTests(unittest.TestCase):
     def setUp(self):
         self.client = CurlClient()
 
-    @patch.object(CurlClient, 'get')
+    @patch.object(CurlClient, "get")
     def test_curl_extracts_url(self, mock_req):
         self.client.curl(
             """
             curl http://localhost:8000/api/v1/poop/
             """
         )
-        mock_req.assert_called_once_with('/api/v1/poop/')
+        mock_req.assert_called_once_with("/api/v1/poop/")
 
-    @patch.object(CurlClient, 'get')
+    @patch.object(CurlClient, "get")
     def test_curl_extracts_quoted_url(self, mock_req):
         self.client.curl(
             """
             curl "http://localhost:8000/api/v1/poop/"
             """
         )
-        mock_req.assert_called_once_with('/api/v1/poop/')
+        mock_req.assert_called_once_with("/api/v1/poop/")
 
-    @patch.object(CurlClient, 'get')
+    @patch.object(CurlClient, "get")
     def test_curl_extracts_get_params(self, mock_req):
         self.client.curl(
             """
             curl 'http://localhost:8000/api/v1/poop/?foo=a&foo=b'
             """
         )
-        mock_req.assert_called_once_with('/api/v1/poop/?foo=a&foo=b')
+        mock_req.assert_called_once_with("/api/v1/poop/?foo=a&foo=b")
 
-    @patch.object(CurlClient, 'head')
+    @patch.object(CurlClient, "head")
     def test_curl_sees_head_implies_head(self, mock_req):
         self.client.curl(
             """
             curl -I http://localhost:8000/neck-topper
             """
         )
-        mock_req.assert_called_once_with('/neck-topper')
+        mock_req.assert_called_once_with("/neck-topper")
 
-    @patch.object(CurlClient, 'post')
+    @patch.object(CurlClient, "post")
     def test_curl_sees_data_implies_post(self, mock_req):
         self.client.curl(
             """
@@ -55,7 +55,7 @@ class CurlTests(unittest.TestCase):
         )
         mock_req.assert_called_once()
 
-    @patch.object(CurlClient, 'post')
+    @patch.object(CurlClient, "post")
     def test_curl_converts_content_type(self, mock_req):
         self.client.curl(
             """
@@ -66,16 +66,17 @@ class CurlTests(unittest.TestCase):
         )
         mock_req.assert_called_once()
         args, kwargs = mock_req.call_args
-        self.assertEqual(args, ('/api/v1/awesome/',))
-        self.assertEqual(kwargs['content_type'], 'application/json')
-        self.assertTrue(kwargs['data'])
+        self.assertEqual(args, ("/api/v1/awesome/",))
+        self.assertEqual(kwargs["content_type"], "application/json")
+        self.assertTrue(kwargs["data"])
 
-    @patch.object(CurlClient, 'get')
+    @patch.object(CurlClient, "get")
     def test_curl_converts_auth(self, mock_req):
         self.client.curl(
             """
             curl http://test:pass@localhost:8000/secret/files/
             """
         )
-        mock_req.assert_called_once_with('/secret/files/',
-                                         HTTP_AUTHORIZATION='Basic dGVzdDpwYXNz')
+        mock_req.assert_called_once_with(
+            "/secret/files/", HTTP_AUTHORIZATION="Basic dGVzdDpwYXNz"
+        )
